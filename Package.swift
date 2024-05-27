e7de273dad1e47abdb14be4a9cd51643eab3feb9
Yuta Saito <kateinoigakukun@gmail.com>
2024-05-27 12:41:34 +0000

    Use upstream JavaScriptKit

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Example",
     dependencies: [
-        .package(url: "https://github.com/swiftwasm/JavaScriptKit", branch: "yt/thread-support"),
+        .package(url: "https://github.com/swiftwasm/JavaScriptKit", revision: "ea069824e205056292f6e50da995975477841432"),
         .package(url: "https://github.com/kateinoigakukun/chibi-ray", revision: "c8cab621a3338dd2f8e817d3785362409d3b8cf1"),
     ],
     targets: [
