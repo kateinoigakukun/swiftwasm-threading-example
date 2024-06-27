34e16e39e6310758e6262bab42b1dc1fc538ebd1
Yuta Saito <kateinoigakukun@gmail.com>
2024-06-27 16:02:24 +0900

    Update JavaScriptKit to use the upstream version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Example",
     dependencies: [
-        .package(path: "../../swiftwasm/JavaScriptKit"),
+        .package(url: "https://github.com/swiftwasm/JavaScriptKit", revision: "0d39aa77cb97e12e3e843254f8d17d6451172a88"),
         .package(url: "https://github.com/kateinoigakukun/chibi-ray", revision: "c8cab621a3338dd2f8e817d3785362409d3b8cf1"),
     ],
     targets: [
